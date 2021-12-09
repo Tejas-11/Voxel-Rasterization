274d6f189e33f2a13d092962e428e61152eb506e
Tejas-11 <tejastaneja11@gmail.com>
2021-12-10 02:18:29 +0530

    multiple updates. Fixed vertical angle to zero because the current ray formula is only correct when vertical angle zero. Added SDL Keyboard Event listners to allow for camera orientation and position changing. Moved from using the modulo operator to using a while loop to limit x, y positions to range. Typecasting negative decimal numbers leads to improper behaviour. 0.5 is type cast to 0 and so is -0.5. The expected behaviour is to type cast -0.5 to -1. To ensure proper behaviour decimal number must be floored before typecasting. Flooring is a costly operation that leads to perfomance reduction. Hence switching to a loop to ensure x and y co-ordinates in range. This way no need to floor and no need to modulo.

--- game.cpp
+++ game.cpp
@@ -16,9 +16,10 @@
 SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
 SDL_Surface* gRenderSurface = NULL;
+SDL_Event e;
 
 // viewer parameters
-static double x=0, y=0, z=150, orih=0, oriv=0;
+static double x=0, y=0, z=150, orih=0;
 
 // map parameters
 static double h2d=1.0;
@@ -34,10 +35,17 @@ static double d2s = 2, dinc = 0.5, dbase;
 static int dlim = 1600;
 
 // optimization variables
+static double cos_orih, sin_orih;
 static double dvar_z[NUMBER_OF_THREADS][SCREEN_HEIGHT], dinc_z[NUMBER_OF_THREADS][SCREEN_HEIGHT];
 static uint8_t sky[3] = {135, 206, 235};
 static double sin_hvspan[SCREEN_WIDTH], cos_hvspan[SCREEN_WIDTH];
 static double tan_vvspan[SCREEN_WIDTH][SCREEN_HEIGHT];
+
+// motion parameters
+static bool fwd=false, bwd=false, rht_ang=false, lft_ang=false, rht=false, lft=false, uup=false, dwn=false;
+static double spd=2.0;
+static double ang_spd=0.01;
+static bool quit=false;
 
 // threading parameters
 typedef struct{
@@ -53,16 +61,13 @@ SDL_Thread *threads[NUMBER_OF_THREADS];
 
 int thread_rasterize(void* data){
     ThreadData *tdata = (ThreadData*)data;
-    int i, j, k;
-    // height begin and end parameters
-    int hb, he, t;
-    unsigned short xt, yt;
+    int i, j, k, t;
+    int xt, yt;
     uint8_t *cptr;
     double *hptr;
     double dvar_x, dvar_y;
     double dinc_x, dinc_y;
-    double cos_theta, sin_theta, tan_phi;
-    double cos_orih, sin_orih, tan_oriv;
+    double cos_theta, sin_theta;
     double *dzvar, *dzinc;
     dzvar = dvar_z[tdata->id];
     dzinc = dinc_z[tdata->id];
@@ -71,35 +76,42 @@ int thread_rasterize(void* data){
     ew = tdata->ew;
     while(tdata->active){
         if(tdata->updated){
-            tan_oriv = tan(oriv);
             sin_orih = sin(orih);
             cos_orih = cos(orih);
             for(j=sw;j<ew;j++){
                 cos_theta = (cos_orih*cos_hvspan[j])-(sin_orih*sin_hvspan[j]);
                 sin_theta = (cos_orih*sin_hvspan[j])+(sin_orih*cos_hvspan[j]);
                 
-                dvar_x = x + dbase*cos_theta;                  // x
-                dinc_x = dinc*cos_theta;                       // x
+                dvar_x = x + dbase*cos_theta;                              // x
+                dinc_x = dinc*cos_theta;                                   // x
                 
-                dvar_y = y + dbase*sin_theta;                  // y
-                dinc_y = dinc*sin_theta;                       // y
+                dvar_y = y + dbase*sin_theta;                              // y
+                dinc_y = dinc*sin_theta;                                   // y
                 for(i=0;i<SCREEN_HEIGHT;i++){
-                    tan_phi = (tan_oriv+tan_vvspan[j][i])/(1-tan_oriv*tan_vvspan[j][i]);
-                    
-                    dzvar[i] = z + dbase*tan_phi;                 // z
-                    dzinc[i] = dinc*tan_phi;                      // z
+                    dzvar[i] = z + dbase*tan_vvspan[j][i];                 // z
+                    dzinc[i] = dinc*tan_vvspan[j][i];                      // z
                 }
                 k=0;
-                hb = 0;
-                he = SCREEN_HEIGHT-1;
-                i = he;
+                i = SCREEN_HEIGHT-1;
                 while(k<dlim){
-                    xt = (unsigned short)(dvar_x)%MAP_WIDTH;
-                    yt = (unsigned short)(dvar_y)%MAP_HEIGHT;
+                    while(dvar_x<0){
+	                    dvar_x += MAP_WIDTH;
+                    }
+                    while(dvar_x>=MAP_WIDTH){
+	                    dvar_x -= MAP_WIDTH;
+                    }
+                    while(dvar_y<0){
+	                    dvar_y += MAP_HEIGHT;
+                    }
+                    while(dvar_y>=MAP_HEIGHT){
+	                    dvar_y -= MAP_HEIGHT;
+                    }
+                    xt = (int)(dvar_x);
+                    yt = (int)(dvar_y);
                     t = yt*MAP_WIDTH + xt;
                     cptr = &cimgarr[0][0][0] + t*3;
                     hptr = &himgarr[0][0] + t;
-                    while(i>=hb && (dzvar[i]+k*dzinc[i])<=(*hptr)){
+                    while(i>=0 && (dzvar[i]+k*dzinc[i])<=(*hptr)){
                         memcpy(fimg[i][j], cptr, 3);
                         i--;
                     }
@@ -111,7 +123,7 @@ int thread_rasterize(void* data){
                     dvar_y += dinc_y;
                 }
                 if(k==dlim){
-                    for(;i>=hb;i--){
+                    for(;i>=0;i--){
                         memcpy(fimg[i][j], sky, 3);
                     }
                 }
@@ -223,6 +235,128 @@ void my_clean(){
     SDL_Quit();
 }
 
+void update(){
+    while(SDL_PollEvent(&e)!=0){
+        if(e.type == SDL_KEYDOWN){
+            switch(e.key.keysym.sym){
+                case SDLK_ESCAPE:
+                quit = true;
+                break;
+
+                case SDLK_UP:
+                fwd = true;
+                break;
+
+                case SDLK_DOWN:
+                bwd = true;
+                break;
+
+                case SDLK_LEFT:
+                lft_ang = true;
+                break;
+
+                case SDLK_RIGHT:
+                rht_ang = true;
+                break;
+
+                case SDLK_w:
+                uup = true;
+                break;
+
+                case SDLK_s:
+                dwn = true;
+                break;
+
+                case SDLK_a:
+                lft = true;
+                break;
+
+                case SDLK_d:
+                rht = true;
+                break;
+            }
+        }
+        else if(e.type == SDL_KEYUP){
+            switch(e.key.keysym.sym){
+                case SDLK_UP:
+                fwd = false;
+                break;
+
+                case SDLK_DOWN:
+                bwd = false;
+                break;
+
+                case SDLK_LEFT:
+                lft_ang = false;
+                break;
+
+                case SDLK_RIGHT:
+                rht_ang = false;
+                break;
+
+                case SDLK_w:
+                uup = false;
+                break;
+
+                case SDLK_s:
+                dwn = false;
+                break;
+
+                case SDLK_a:
+                lft = false;
+                break;
+
+                case SDLK_d:
+                rht = false;
+                break;
+            }
+        }
+    }
+
+    if(fwd){
+        x += spd*cos_orih;
+        y += spd*sin_orih;
+    }
+    if(bwd){
+        x -= spd*cos_orih;
+        y -= spd*sin_orih;
+    }
+    if(lft){
+        x -= spd*sin_orih;
+        y += spd*cos_orih;
+    }
+    if(rht){
+        x += spd*sin_orih;
+        y -= spd*cos_orih;
+    }
+    if(uup){
+        z += spd;
+    }
+    if(dwn){
+        z -= spd;
+    }
+    if(lft_ang){
+        orih += ang_spd;
+    }
+    if(rht_ang){
+        orih -= ang_spd;
+    }
+
+    while(x<0){
+        x += MAP_WIDTH;
+    }
+    while(x>=MAP_WIDTH){
+        x -= MAP_WIDTH;
+    }
+    while(y<0){
+        y += MAP_HEIGHT;
+    }
+    while(y>=MAP_HEIGHT){
+        y -= MAP_HEIGHT;
+    }
+}
+
+
 int main(){
     using std::chrono::high_resolution_clock;
     using std::chrono::duration_cast;
@@ -231,10 +365,9 @@ int main(){
     if(!my_setup()){
         return 0;
     }
-    int i;
-    int j;
+    int i=0, j;
     auto t1 = high_resolution_clock::now();
-    for(i=0;i<50;i++){
+    while(!quit){
         for(j=0;j<NUMBER_OF_THREADS;j++){
             threads_data[j].updated = true;
         }
@@ -246,12 +379,13 @@ int main(){
         gRenderSurface = SDL_CreateRGBSurfaceFrom((void*)fimg, SCREEN_WIDTH, SCREEN_HEIGHT, 24, 3*SCREEN_WIDTH, 0x000000ff, 0x0000ff00, 0x00ff0000, 0);
         SDL_BlitSurface(gRenderSurface, NULL, gScreenSurface, NULL);
         SDL_UpdateWindowSurface(gWindow);
-        x += 1;
+        update();
+        i++;
     }
     auto t2 = high_resolution_clock::now();
 
     /* Getting number of milliseconds as a double. */
-    duration<double, std::milli> du1 = (t2 - t1)/50;
+    duration<double, std::milli> du1 = (t2 - t1)/i;
     std::cout << du1.count() << "ms\n";
 
     my_clean();
